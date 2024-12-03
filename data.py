004ada946f69bcdc2d7733b40e456b2e51896f48
yo-its-anas <anasejaz4042@gmail.com>
2024-12-03 14:52:26 +0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,6 @@
+# data.py
+blood_banks = [
+    {"name": "Clifton Blood Bank", "location": "Clifton", "contact": "021-1234567", "distance": 2.5},
+    {"name": "Saddar Blood Bank", "location": "Saddar", "contact": "021-9876543", "distance": 3.0},
+    # Add more dummy data for 20-25 areas
+]
