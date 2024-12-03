4837efe1e8fe4aadad42e02e05f4e0b325b3909e
yo-its-anas <anasejaz4042@gmail.com>
2024-12-03 15:00:52 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,19 +1,30 @@
-# auth.py
 import json
+import os
 
 def create_user(username, password, email, phone, location, blood_group):
+    if not os.path.exists("users.json"):
+        with open("users.json", "w") as file:
+            json.dump({}, file)
+    
     with open("users.json", "r+") as file:
         users = json.load(file)
         if username in users:
             return "Username already exists."
-        users[username] = {"password": password, "email": email, "phone": phone, "location": location, "blood_group": blood_group}
+        users[username] = {
+            "password": password,
+            "email": email,
+            "phone": phone,
+            "location": location,
+            "blood_group": blood_group
+        }
         file.seek(0)
         json.dump(users, file, indent=4)
         return "User created successfully!"
 
-def authenticate_user(username, password):
-    with open("users.json", "r") as file:
-        users = json.load(file)
-        if username in users and users[username]["password"] == password:
-            return True
-        return False
+def login_user(username, password):
+    if os.path.exists("users.json"):
+        with open("users.json", "r") as file:
+            users = json.load(file)
+            if username in users and users[username]["password"] == password:
+                return True
+    return False
